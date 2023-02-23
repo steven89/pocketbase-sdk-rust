638177d1d2030294be7636c1384813abe94f5819
Steven Salaun <steven.salaun@gmail.com>
2023-02-23 15:15:24 +0100

    serde for UserTypes

--- src/user/mod.rs
+++ src/user/mod.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash,Serialize,Deserialize)]
 pub enum UserTypes {
     User,
     Admin
